055ac357f1d8ceec8db409d1b442fad33ffc675c
bachtiarpanjaitan <bachtiarpanjaitan0@gmail.com>
2020-03-08 13:34:23 +0700

    add some code

--- src/Bataxdevino.cpp
+++ src/Bataxdevino.cpp
@@ -1,7 +1,31 @@
 #include "Bataxdevino.h"
 #include <LiquidCrystal.h>
-void Bataxdevino::lcdPrint(LiquidCrystal lcd,int row, int col, char* msg, bool clear = false){
+#include <Arduino.h>
+void Bataxdevino::lcdPrint(LiquidCrystal lcd,int row, int col, char* msg, bool clear = false)
+{
 	if(clear == true) lcd.clear();
 	lcd.setCursor(col,row);
   	lcd.print(msg);
-}	
+}
+
+/**
+ * @param pin read analog pin button
+ **/
+int Bataxdevino::readLcdButton(int pin)
+{
+	int result;
+	int x;
+	x = analogRead(pin);
+	if(x<60){         //right
+		result = 0;
+	}else if(x<200){  //up
+		result = 1;
+	}else if(x<400){  //down
+		result = 2;
+	}else if(x<600){  //left
+		result = 3;
+	}else if(x<800){  //select
+		result = 4;
+	}
+	return(result);
+}
